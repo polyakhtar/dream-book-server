08e82912650bdbeb3c5cdcaeceac53a8ca1f42b3
polyakhtar <poly09876pm@gmail.com>
2022-12-28 10:52:01 +0600

    post api create

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express=require('express');
 const cors=require('cors');
 const app=express();
@@ -7,6 +8,28 @@ const port=process.env.PORT||5000;
 // middlware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mjqzqbo.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+try{
+    const postCollection=client.db('dUser').collection('postCollection');
+
+    app.post('/posts',async(req,res)=>{
+        const post=req.body;
+        const reault=await postCollection.insertOne(post);
+        res.send(post);
+    })
+}
+finally{
+
+}
+}
+run().catch(error=>console.error(error))
 
 app.get('/',(req,res)=>{
     res.send('server is running')
